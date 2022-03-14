4adbaae3c6ef4c9b3de61ffe3f1d40738ad7bd66
Rock Lee <rock@RockdeMacBook-Pro.local>
2022-03-14 11:24:41 +0800

    add

--- lib/common/utils/http_server.dart
+++ lib/common/utils/http_server.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
+
 class HttpServer {
   static final HttpServer _instance = HttpServer._internal();
   factory HttpServer() => _instance;
 
   HttpServer._internal() {
-    print('object');
+    debugPrint('object');
   }
 }
